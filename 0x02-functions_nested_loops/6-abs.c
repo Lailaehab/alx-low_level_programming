e06bc24601c3fb85b8bd2b833314f39a0b870d98
Lailaehab <lailaihab24@gmail.com>
2023-06-20 11:21:10 -0700

    f

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs -compute absolute value
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		printf("%d", -1*n);
+	}
+	else
+		printf("%d", n);
+	return (0);
+}
