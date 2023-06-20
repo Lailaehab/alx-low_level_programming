b26f69d0fcac2973a91b08af4d1e4781fc07f590
Lailaehab <lailaihab24@gmail.com>
2023-06-20 10:23:27 -0700

    d

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main -check the code
+ * Returm: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <10; i++)
+		print_alphabet();
+	return (0);
+}
