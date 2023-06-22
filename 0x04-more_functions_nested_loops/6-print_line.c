4f8c0728d1ce4cad1a067cafa3db862c2e7ecd23
Lailaehab <lailaihab24@gmail.com>
2023-06-22 16:24:19 -0700

    d

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,2 +1,19 @@
 #include "main.h"
 
+/**
+ * print_line -prints line
+ * @n: number of times the _ should be printed
+ */
+void print_line(int n)
+{
+	int l;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (l = 1; l <= n; l++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
