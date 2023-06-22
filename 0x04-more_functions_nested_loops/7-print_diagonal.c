d3f6db80ce69f59c38da21cd79a1d1a08166ba3c
Lailaehab <lailaihab24@gmail.com>
2023-06-22 16:31:32 -0700

    f

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal
+ * @n : number of times \ should be printed
+ */
+void print_diagonal(int n)
+{
+	int pos, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (pos = 1; pos <= n; pos++)
+		{
+			for (space = 1; space <= pos; space++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
+
