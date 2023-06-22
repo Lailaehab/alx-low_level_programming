d5dee4c727fbe4754c892f57dcb7e439ce538326
Lailaehab <lailaihab24@gmail.com>
2023-06-22 14:05:03 -0700

    f

--- 0x03-debugging/0x04-more_functions_nested_loops/0-isupper.c
+++ 0x03-debugging/0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _isupper - check if letter is uppercase
  * @c: input
@@ -8,5 +9,6 @@ int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
-	return (0);
+	else
+		return (0);
 }
