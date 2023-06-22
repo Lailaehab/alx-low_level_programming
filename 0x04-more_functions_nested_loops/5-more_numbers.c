2b3368d2c7e08cf5022fbce5508f707a8947ffaf
Lailaehab <lailaihab24@gmail.com>
2023-06-22 15:54:44 -0700

    d

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,21 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int n;
 
 	for (i = 0; i < 10; i++)
 	{
+		n = j;
 		for (j = 0; j < 15; j++)
-			_putchar(j + '0');
-		_putchar('\n');
+		{
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				n = j % 10;
+			}
+			_putchar(n + '0');
+		}
+
 	}
 	_putchar('\n');
 }
