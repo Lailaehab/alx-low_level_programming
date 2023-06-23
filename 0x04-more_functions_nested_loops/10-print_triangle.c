488477c81e10a5b5bf92c20007275982dbd656c4
Lailaehab <lailaihab24@gmail.com>
2023-06-22 17:21:00 -0700

    f

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,7 +15,7 @@ void print_triangle(int size)
 	{
 		for (x = 0; x < size; x++)
 		{
-			for (y = size-x; y > 1; y--)
+			for (y = size - x; y > 1; y--)
 				_putchar(32);
 			for (z = 0; z <= x; z++)
 				_putchar(35);
