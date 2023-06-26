dcf2075a60538caa4a4e22467f81a41e571d3dd1
Lailaehab <lailaihab24@gmail.com>
2023-06-26 14:48:51 -0700

    f

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,7 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = 0; *s != '\0'; i++)
-	{
-		s++;
-	}
-
+	for (i = 0; s[i] != '\0'; i++)
 	for (i = i - 1; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
