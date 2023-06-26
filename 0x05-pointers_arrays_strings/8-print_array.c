67c3ba13c65348bf4d068b531a1d48812e926b2e
Lailaehab <lailaihab24@gmail.com>
2023-06-26 16:36:59 -0700

    f

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - gjgbj
+ * @a: hkn
+ * @n: fgh
+ * Return: ghnjmk
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n");
+}
