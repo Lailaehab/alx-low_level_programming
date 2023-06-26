85f8e1f26b2b4601759353455cb2cacb89e1723a
Lailaehab <lailaihab24@gmail.com>
2023-06-26 16:31:06 -0700

    f

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts_half - cjgvk
+ * @str: gh
+ * Return: hjkv
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
