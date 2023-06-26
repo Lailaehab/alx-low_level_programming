b94baa5101e145be06dc35d671fdea3d5b47c5a3
Lailaehab <lailaihab24@gmail.com>
2023-06-26 16:05:48 -0700

    d

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,13 +7,15 @@
  */
 void rev_string(char *s)
 {
-	int i = 0;
+	int i, j;
+	char temp;
 
-	while (s[i])
-		i++;
-	while (i--)
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	for (i = 0, j = i + 1; i < j; i++, j--)
 	{
-		_putchar(s[i]);
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
 	}
-	_putchar('\n');
 }
