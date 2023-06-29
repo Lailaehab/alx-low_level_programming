c47caa9ead617ee270a1378e8e03e94583a94adb
Lailaehab <lailaihab24@gmail.com>
2023-06-29 13:26:04 -0700

    f

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - fgh
+ * @a: tgui
+ * @n: gyjkl
+ * Return: fghj
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, t;
+
+	for (i = 0; j = (n - 1); i < j; i++, j--)
+	{
+		t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+}
