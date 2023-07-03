94ca37f2ce3051000cf403328078b9442d827e79
Lailaehab <lailaihab24@gmail.com>
2023-07-03 14:06:54 -0700

    g

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strspn - fgh
+ * @s: gh
+ * @accept: m
+ * Return : Always 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int itr, jtr;
+
+	for (itr = 0; s[itr] != '\0'; itr++)
+	{
+		for (jtr = 0; accept[jtr] != '\0'; jtr++)
+		{
+			if (accept[jtr] == '\0')
+				return (itr);
+		}
+	}
+	return (itr);
+}
