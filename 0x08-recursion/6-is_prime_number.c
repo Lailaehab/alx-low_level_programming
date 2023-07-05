dfa249f01fd291fc6f809487b1c7caa088aacf9b
Lailaehab <lailaihab24@gmail.com>
2023-07-05 14:46:53 -0700

    f

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - dfgh
+ * @n: ghj
+ * Return: fghj
+ */
+int check_prime(int n, int oth);
+int is_prime_number(int n)
+{
+	return (check_prime(n, 2));
+}
+
+/**
+ * check_prime - fghj
+ * @n: fghj
+ * @oth: fghj
+ * Return: fghj
+ */
+int check_prime(int n, int oth)
+{
+	if (oth >= n && n > 1)
+		return (1);
+	else if (n % oth == 0 || n <= 1)
+		return (0);
+	else
+		return (check_prime(n, oth + 1));
+}
