8063898541224a5c5bcb26bd14558c713e4f145a
Lailaehab <lailaihab24@gmail.com>
2023-07-07 15:47:53 -0700

    j

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - ghjk
+ * @argc: ghjk
+ * @argv: thjkl
+ * Return: ghjk
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
