47ad6a8953e21d57d1268b5d881b1687397b491a
Lailaehab <lailaihab24@gmail.com>
2023-07-07 15:24:45 -0700

    g

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - hjd
+ * @argc: ghjk
+ * @argv: fghjk
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argc[0]);
+	return (0);
+}
