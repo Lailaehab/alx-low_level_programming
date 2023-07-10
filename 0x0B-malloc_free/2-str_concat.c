7745872adf8e95241af8d156ac35400184d3ab91
Lailaehab <lailaihab24@gmail.com>
2023-07-10 14:47:42 -0700

    g

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,10 @@ char *str_concat(char *s1, char *s2)
 	int size1 = 0, size2 = 0, i = 0, j;
 	char *m;
 
+	if (s1 == NULL || s2 == NULL)
+	{
+		return NULL;
+	}
 
 	for (; s1[size1] != '\0'; size1++)
 		;
