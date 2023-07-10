0bb982a9e9b100566ace9d06df5159860b4d250e
Lailaehab <lailaihab24@gmail.com>
2023-07-10 14:43:34 -0700

    f

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * str_concat - fghj
+ * @s1: ghj
+ * @s2: fghj
+ * Return: ghj
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int size1 = 0, size2 = 0, i = 0, j;
+	char *m;
+
+
+	for (; s1[size1] != '\0'; size1++)
+		;
+	for (; s2[size2] != '\0'; size2++)
+		;
+	m = malloc(size1 + size2 + 1);
+	if (m == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		for (; i < size1; i++)
+			m[i] = s1[i];
+		for (j = 0; i < size1 + size2; i++)
+		{
+			m[i] = s2[j];
+			j++;
+		}
+	}
+	return (m);
+}
