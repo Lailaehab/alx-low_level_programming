3d247daf6c125aa0d5b658ec4ef5e94ae12fa816
Lailaehab <lailaihab24@gmail.com>
2023-07-10 16:40:18 -0700

    fg

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * free_grid - fghj
+ * @grid: ghj
+ * @height: ghj
+ * Return: ghj
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (; i < height; i++)
+		free(grid[i]);
+	free{grid};
+}
