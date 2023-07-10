56247f99ce80948b552e0abd5c6b1e142413fe9b
Lailaehab <lailaihab24@gmail.com>
2023-07-10 16:34:57 -0700

    gf

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * alloc_grid - hj
+ * @width: ghj
+ * @height: fghj
+ * Return: fghj
+ */
+int **alloc_grid(int width, int height)
+{
+	int **m, i, j;
+
+	m = malloc(sizeof(*m) * height);
+
+	if (width <= 0 || height <= 0 || m == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			m[i] = malloc(sizeof(**m) * width);
+			if (m[i] == 0)
+			{
+				while (i--)
+				{
+					free(m[i]);
+				}
+				free(m);
+				return (NULL);
+			}
+			for (j = 0; j < width; j++)
+			{
+				m[i][j] = 0;
+			}
+		}
+	}
+	return (m);
+}
