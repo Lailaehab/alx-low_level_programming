5e40a2a86780e16498f1268ace5833b043818f99
Lailaehab <lailaihab24@gmail.com>
2023-07-13 15:20:14 -0700

    g

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * malloc_checked - ghjkl
+ * @b: gthj
+ * Return: ghyj
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *m = malloc(b);
+
+	if (m == 0)
+		exit(98);
+	return (m);
+}
