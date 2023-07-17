4017c8b5a1c8c229feebbbfad56e8818fe6b2fb0
Lailaehab <lailaihab24@gmail.com>
2023-07-17 13:09:20 -0700

    d

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - fghj
+ * @name: fghj
+ * @age: fghj
+ * @owner: rty
+ * Description: fgh
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+
+};
+
+#endif
