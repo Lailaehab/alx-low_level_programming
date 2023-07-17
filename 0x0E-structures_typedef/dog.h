50a8a6d086d07be2c67cd772f30010b4fc8b2c90
Lailaehab <lailaihab24@gmail.com>
2023-07-17 13:15:55 -0700

    d

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,6 +8,7 @@
  * @owner: rty
  * Description: fgh
  */
+typedef struct dog dog_t;
 struct dog
 {
 	char *name;
@@ -15,5 +16,9 @@ struct dog
 	float age;
 
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
