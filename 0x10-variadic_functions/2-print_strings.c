c17f82ce5492c356c74fe7b4f26d0e5f13cc9488
Lailaehab <lailaihab24@gmail.com>
2023-07-20 06:20:20 -0700

    df

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - dfgh
+ * @separator: dfgh
+ * @n: sdfg
+ * @...: fghj
+ * Return: sd
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	char *str;
+	va_list ap;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ap, n);
+	while (i--)
+	{
+		printf("%s%s", (str = va_arg(ap, char*)) ? str : "(nil)",
+				i ? (separator ? separator : "") : "\n");
+	}
+	va_end(ap);
+}
