52d9abd37b0eb4f46f3e06bd46bf7e9371667417
Lailaehab <lailaihab24@gmail.com>
2023-07-21 16:18:06 -0700

    j

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "calc.h"
+
+/**
+ * op_add - sdfg
+ * @a: dfgh
+ * @b: ghj
+ * Return: sd
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sdfgh
+ * @a: fghj
+ * @b: sdfg
+ * Return: dfgh
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - sdfgh
+ * @a: fghj
+ * @b: sdfg
+ * Return: dfgh
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - sdfgh
+ * @a: fghj
+ * @b: sdfg
+ * Return: dfgh
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - sdfgh
+ * @a: fghj
+ * @b: sdfg
+ * Return: dfgh
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
