77bc8e61a27736db63f7281bb053a55204258856
LailaEhab <lailaihab24@gmail.com>
2023-07-27 16:15:51 -0700

    h

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - asdfghj
+ * @h: asdfg
+ *
+ * Return: xcvbnm
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h h->next;
+		i++;
+	}
+	
+	return (i);
+}
