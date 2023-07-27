dfbc73db8f845411eb4fcb676708279d14bbe577
LailaEhab <lailaihab24@gmail.com>
2023-07-27 16:11:21 -0700

    f

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+
+/**
+ * _strlen - dfghjk
+ * @s: sdfgh
+ * Return: top
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - fghjkl
+ * @h: asdfg
+ * Return: tyui
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
